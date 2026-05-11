4546436e1a0a1e9d6d1dc75f5dfd920aa1bc9e7a
igorabacate <i.ritzelpacheco@gmail.com>
2026-05-10 21:28:01 -0300

    this python code splits an image into red, green, blue and yellow color channels

--- codigos/py/split_ryb.py
+++ codigos/py/split_ryb.py
@@ -1,25 +1,24 @@
 import cv2
-import numpy as np
+import matplotlib.pyplot as plt
 
-# Carregue a imagem
-imagem = cv2.imread('Documentos/fluorescencia_ma/02_07_25_2.jpg')
+# Load image
+img = cv2.cvtColor(cv2.imread('exemplo.png'), cv2.COLOR_BGR2RGB)
 
-# Separe os canais BGR
-b_canal, g_canal, r_canal = cv2.split(imagem)
+# Separate channels
+r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
+y = (r.astype(float) + g.astype(float)) / 2  # Yellow channel
 
-# Crie o canal amarelo (RYB)
-y_canal = np.clip((r_canal + g_canal) / 2, 0, 255).astype(np.uint8)
+# Plot all channels
+fig, ax = plt.subplots(2, 2, figsize=(10, 8))
+for i, (channel, title, color) in enumerate([
+    (r, 'Red', 'Reds'),
+    (g, 'Green', 'Greens'), 
+    (b, 'Blue', 'Blues'),
+    (y, 'Yellow', 'YlOrBr')
+]):
+    ax[i//2, i%2].imshow(channel, cmap=color)
+    ax[i//2, i%2].set_title(title, fontsize=12)
+    ax[i//2, i%2].axis('off')
 
-# Crie o canal verde (RYB)
-g_canal = np.clip((y_canal + b_canal) / 2, 0, 255).astype(np.uint8)
-
-# Exiba os canais RYB (usando as funções do OpenCV que esperam BGR)
-cv2.imshow('Vermelho (RYB)', r_canal)
-cv2.imshow('Amarelo (RYB)', y_canal)
-cv2.imshow('Azul (RYB)', b_canal)
-cv2.imshow('Verde (RYB)', g_canal)
-
-# Aguarde uma tecla para fechar as janelas
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+plt.tight_layout()
+plt.show()
